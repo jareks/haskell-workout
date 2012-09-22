0d7f2369c312d0809ab6738f5ac8e3959daee448
Jarosław Skrzypek <skrzypek.jarek@gmail.com>
2012-09-22 15:37:40 +0200

    refactored primes500 into something less scary

--- prime500/prime500.hs
+++ prime500/prime500.hs
@@ -1,46 +1,24 @@
 import Debug.Trace
 
 main = do
-  print (is_prime [] 2)
-  print (is_prime [2] 3)
-  print (is_prime [2,3] 4)
-
-  print (get_primes 500 [3]) 
+  print (get_primes 500) 
   return ()
 
-get_primes :: (Show a) => (Integral a) => a -> [a] -> [a]
---get_primes n primes | trace ("get_primes " ++ show primes ++ " " ++ show n) False = undefined
-get_primes 0 primes = primes
-get_primes n primes = 
-  get_primes (n - 1) (primes++[(next_prime primes)])
+get_primes :: (Show a, Integral a) => a -> [a]
+get_primes 1 = [2]
+get_primes 2 = [2,3]
+get_primes n = 
+  foldl (\primes i -> primes ++ [(next_prime primes)]) (get_primes 2) [2..n]
 
-next_prime :: (Show a) => (Integral a) => [a] -> a
+next_prime :: (Show a, Integral a) => [a] -> a
 --next_prime primes | trace ("next_prime " ++ show primes) False = undefined
 next_prime primes = let
-    c = (last primes) + 2
+    last_prime = last primes
   in
-    if is_prime primes c
-    then
-      c
-    else
-      next_prime_after primes c
-
-next_prime_after :: (Show a) => (Integral a) => [a] -> a -> a
---next_prime_after primes c | trace ("next_prime_after " ++ show primes ++ " " ++ show c) False = undefined
-next_prime_after primes c = 
-  if is_prime primes (c + 2)
-  then
-    c + 2
-  else
-    next_prime_after primes (c + 2)
+    head $ filter (\num -> is_prime primes num) [last_prime, last_prime + 2..]
 
 is_prime :: (Integral a) => (Show a) => [a] -> a -> Bool
 is_prime primes n =
-  foldl (\acc prime -> 
-   -- trace 
-   --   ("mod "  ++ show n ++ " mod " ++ show prime ++ " = "++ show (n `mod` prime)) 
-        (if n `mod` prime == 0 then False else acc)
-      ) 
-      True primes
+  foldl (\acc prime -> (if n `mod` prime == 0 then False else acc)) True primes
     
   
